91c59cd60d1de54022bf75fd5a1013552a7efaf7
antonio <antoniomanzari@gmail.com>
2021-01-25 15:04:15 -0500

    added comments with TODO list

--- GC_Lab7_RegEx/Program.cs
+++ GC_Lab7_RegEx/Program.cs
@@ -4,9 +4,23 @@ namespace GC_Lab7_RegEx
 {
     class Program
     {
+        // GC Lab 7 Regular Expressions
+        // Antonio Manzari
+
+        // TODO
+        // Write a method that will validate names. Names can only have alphabets, they should start with a capital letter, and they have a max length of 30. (but what about people with SUPER LONG names?)
+        // Write a method that will validate emails. An email should be in the following format: {comb of alphanumeric chars, with a length between 5 and 30, and there are no special chars}@{comb of alpnum chars, with a length between 5 and 10, and there are no special chars}.{domain can be a comb of alphnum chars with a length of two or three}
+        // Write a method that will validate phone numbers. A phone # should be in the format: {area code of 3 dig}-{3dig}-{4dig}
+        // Write a method that will validate date based on the following formt: (dd/mm/yyyy)
+
+        // NAME
+        // EMAIL
+        // PHONE #
+        // DATE
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
