d8c787ae5b4cae0f87a36eb2f8cbd3d3cad437fc
antonio <antoniomanzari@gmail.com>
2021-01-26 19:03:06 -0500

    added email method, refactored

--- GC_Lab7_RegEx/Program.cs
+++ GC_Lab7_RegEx/Program.cs
@@ -14,7 +14,7 @@ namespace GC_Lab7_RegEx
         // Write a method that will validate emails. An email should be in the following format: {comb of alphanumeric chars, with a length between 5 and 30, and there are no special chars}@{comb of alpnum chars, with a length between 5 and 10, and there are no special chars}.{domain can be a comb of alphnum chars with a length of two or three}
         // Write a method that will validate phone numbers. A phone # should be in the format: {area code of 3 dig}-{3dig}-{4dig}
         // Write a method that will validate date based on the following formt: (dd/mm/yyyy)
-        // EX
+        // EXTRA
         // Write a method that validates HTML elements
 
         // O  NAME
@@ -23,7 +23,37 @@ namespace GC_Lab7_RegEx
         // O  DATE
         // O  EX: HTML ELEMENTS
 
-        public static List<string> phoneTestNumbers= new List<string>
+        
+;        static void Main(string[] args)
+        {
+            //RunTestIsValidPhoneNum();
+
+        }
+
+        #region METHODS
+       
+        public static bool IsValidPhoneNumber(string userPhoneNumber)
+        {
+            string phonePattern = @"^\d{3}-\d{3}-\d{4}$";
+
+            return Regex.IsMatch(userPhoneNumber, phonePattern);
+        }
+
+        public static bool IsValidEmail(string userEmail)
+        {
+            //TODO dont allow special characters?
+            string emailPattern = @"^[A-z0-9]{5,30}[@][A-z0-9]{5,10}[.][A-z]{2,3}$";
+
+            return Regex.IsMatch(userEmail, emailPattern) ;
+        }
+
+        #endregion
+
+        #region TESTS
+        
+        private static void RunTestIsValidPhoneNum()
+        {
+            List<string> phoneTestNumbers = new List<string>
             {
                 "234-44-2434",
                 "1231231234",
@@ -32,19 +62,12 @@ namespace GC_Lab7_RegEx
                 "(249)230-1111"
             };
 
-        static void Main(string[] args)
-        {
             foreach (var testNumber in phoneTestNumbers)
             {
                 Console.WriteLine(IsValidPhoneNumber(testNumber));
             }
         }
-
-        public static bool IsValidPhoneNumber(string phoneNumber)
-        {
-            string phonePattern = @"^\d\d\d-\d\d\d-\d\d\d\d$";
-
-            return Regex.IsMatch(phoneNumber, phonePattern);
-        }
+       
+        #endregion
     }
 }
